bdcbeea297e733d8a5e245a50141abbca7914185
Jakub Szafranski <125590537+JS555555@users.noreply.github.com>
2023-04-20 16:18:01 +0200

    Update api.py

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ import joblib
 import numpy as np
 from flask import Flask, jsonify, request
 from covertype_class import CovertypeClassifier
+from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 model = CovertypeClassifier()
@@ -10,19 +11,25 @@ model = CovertypeClassifier()
 def predict():
     content = request.json
     input_data = content['input_data']
+
+    # Scale input data
+    scaler = StandardScaler()
+    input_data_scaled = scaler.fit_transform(input_data)
+
     model = content['model_name']
+    
 
     if model == 'lr':
         lr = joblib.load('lr_model.joblib')
-        pred = (lr.predict(input_data)).tolist()
+        pred = (lr.predict(input_data_scaled)).tolist()
 
     elif model == 'dtc':
         dtc = joblib.load('dtc_model.joblib')
-        pred = (dtc.predict(input_data)).tolist()
+        pred = (dtc.predict(input_data_scaled)).tolist()
 
     elif model == 'ann':
         ann = joblib.load('ann_model.joblib')
-        pred = (np.argmax(ann.predict(input_data), axis=1)).tolist()
+        pred = (np.argmax(ann.predict(input_data_scaled), axis=1)).tolist()
 
     elif model == 'heuritic':
         clf = CovertypeClassifier()
